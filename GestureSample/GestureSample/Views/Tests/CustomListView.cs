3a6e80ef3b4346e96c4a0a210c2509d4b4957fb7
mr <michael@mrumpler.at>
2016-06-30 17:22:43 +0200

    add missing files

---
+++ GestureSample/GestureSample/Views/Tests/CustomListView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestureSample.Views.Tests
+{
+    public class CustomListView : MR.Gestures.ListView
+    {
+        public CustomListView() : base(Xamarin.Forms.ListViewCachingStrategy.RecycleElement)
+        {
+        }
+    }
+}
---
+++ GestureSample/GestureSample/Views/Tests/CustomListViewPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using GestureSample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GestureSample.Views.Tests
+{
+    public partial class CustomListViewPage : ContentPage
+    {
+        public CustomListViewPage()
+        {
+            InitializeComponent();
+        }
+
+        void Cell_Down(object sender, MR.Gestures.DownUpEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Down method called with " + e.TriggeringTouches.Length + " fingers");
+        }
+
+        void Cell_Up(object sender, MR.Gestures.DownUpEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Up method called with " + e.TriggeringTouches.Length + " fingers");
+        }
+
+        void Cell_Tapping(object sender, MR.Gestures.TapEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Tapping method called " + e.NumberOfTaps + " times");
+        }
+
+        void Cell_Tapped(object sender, MR.Gestures.TapEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Tapped method called " + e.NumberOfTaps + " times");
+        }
+
+        void Cell_DoubleTapped(object sender, MR.Gestures.TapEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_DoubleTapped method called " + e.NumberOfTaps + " times");
+        }
+
+        void Cell_LongPressing(object sender, MR.Gestures.LongPressEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_LongPressing method called");
+        }
+
+        void Cell_LongPressed(object sender, MR.Gestures.LongPressEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_LongPressed method called");
+        }
+
+        void Cell_Panning(object sender, MR.Gestures.PanEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Panning method called");
+        }
+
+        void Cell_Panned(object sender, MR.Gestures.PanEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Panned method called");
+        }
+
+        void Cell_Swiped(object sender, MR.Gestures.SwipeEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Swiped method called, swiped " + e.Direction);
+        }
+
+        void Cell_Pinching(object sender, MR.Gestures.PinchEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Pinching method called");
+        }
+
+        void Cell_Pinched(object sender, MR.Gestures.PinchEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Pinched method called");
+        }
+
+        void Cell_Rotating(object sender, MR.Gestures.RotateEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Rotating method called");
+        }
+
+        void Cell_Rotated(object sender, MR.Gestures.RotateEventArgs e)
+        {
+            ((TextOnlyViewModel)BindingContext).AddText("Cell_Rotated method called");
+        }
+    }
+}
