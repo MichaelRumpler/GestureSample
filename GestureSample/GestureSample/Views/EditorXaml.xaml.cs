e2c2b4288a5c0be789d47c4f105bec0989c7f317
mr <michael@mrumpler.at>
2016-12-15 08:58:53 +0100

    add some XF.Editor events

--- GestureSample/GestureSample/Views/EditorXaml.xaml.cs
+++ GestureSample/GestureSample/Views/EditorXaml.xaml.cs
@@ -1,4 +1,7 @@
 ﻿
+using GestureSample.ViewModels;
+using System;
+
 namespace GestureSample.Views
 {
 	public partial class EditorXaml
@@ -6,6 +9,21 @@ namespace GestureSample.Views
 		public EditorXaml()
 		{
 			InitializeComponent();
+		}
+
+		private void Editor_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+		{
+			AddText("TextChanged");
+		}
+
+		private void Editor_Completed(object sender, EventArgs e)
+		{
+			AddText("Completed");
+		}
+
+		private void AddText(string text)
+		{
+			((TextOnlyViewModel)BindingContext).AddText(text);
 		}
 	}
 }
