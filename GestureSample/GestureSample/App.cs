05251a5e38f779e6467091cae97f658599280756
mr <michael@mrumpler.at>
2018-01-30 15:17:35 +0100

    remove empty overrides

--- GestureSample/GestureSample/App.cs
+++ GestureSample/GestureSample/App.cs
@@ -27,20 +27,5 @@ namespace GestureSample
 			var mainPage = new MainPage() { Title = "Control Categories", BindingContext = samplePages };
 			MainPage = MainNavigation = new NavigationPage(mainPage);
 		}
-
-		protected override void OnStart()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume()
-		{
-			// Handle when your app resumes
-		}
 	}
 }
